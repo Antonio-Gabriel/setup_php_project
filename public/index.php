930e9c1b7ce61992884ae5bc99cd362cf8c54f41
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-10-15 20:34:14 +0100

    chore: configure containers of slim

--- public/index.php
+++ public/index.php
@@ -1,13 +1,24 @@
 <?php
 
-require_once dirname(__DIR__) . "/vendor/autoload.php";
-// require_once dirname(__DIR__). "/src/routes/index.php";
+require __DIR__ . '/../bin/php_sapi.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
 
 use Slim\App;
-$app = new App();
 
-$app->get("/", function($req, $res) {
-    echo "test";
-});
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new App($settings);
 
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require_once __DIR__ . '/../src/routes.php';
+
+// Run app
 $app->run();
